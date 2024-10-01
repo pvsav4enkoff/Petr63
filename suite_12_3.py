eb237d3ffe21faa2c38f1c260d38ffc6234f2d44
pvsav4enkoff <pvsav4enkoff@gmail.com>
2024-10-01 10:06:21 +0400

    Update suite_12_3.py

--- suite_12_3.py
+++ suite_12_3.py
@@ -1,14 +1,14 @@
 import unittest
-from tests_12_3 import TournamentTest2
+from tests_12_1 import RunnerTest
 from tests_12_2 import TournamentTest
-# from test_12_3 import TournamentTest
 
 # Создание TestSuite
 suite = unittest.TestSuite()
 
 # Добавление тестов в TestSuite
+
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(RunnerTest))
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TournamentTest))
-suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TournamentTest2))
 
 # Создание объекта TextTestRunner
 runner = unittest.TextTestRunner(verbosity=2)
