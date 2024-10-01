f3c51d3a8e5bcee21f990b0732c6e7085a0574d2
pvsav4enkoff <pvsav4enkoff@gmail.com>
2024-10-01 10:07:37 +0400

    Update tests_12_4.py

--- tests_12_4.py
+++ tests_12_4.py
@@ -1,13 +1,13 @@
 import logging
 import unittest
-from tests_12_2 import TournamentTest
+from test_runner import RunnerTest
 
 suite = unittest.TestSuite()
 
-suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TournamentTest))
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(RunnerTest))
 
 
-logging.basicConfig(level=logging.INFO,filemode='w',filename='runner_tests.log',format="%(asctime)s | %(levelname)s | %(message)s",encoding="utf-8")
+logging.basicConfig(level=logging.INFO,filemode='w',filename='runner_tests.log',format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",encoding="utf-8")
 
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(suite)
