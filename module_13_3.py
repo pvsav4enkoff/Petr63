8063f3dd7c8411f69247f3c02c6d344015811424
pvsav4enkoff <pvsav4enkoff@gmail.com>
2024-10-06 12:52:47 +0400

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api =  "7874058890:AAG5ytMjLAppzNZG2rSGtlci-sIPUYJbDx0"
+bot=Bot(token=api)
+dp=Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands="start")
+async def start_message(message: types.Message):
+    await asyncio.create_task(message.answer('Привет! Я бот помогающий твоему здоровью.'))
+
+
+@dp.message_handler()
+async def all_message(message):
+    await asyncio.create_task(message.answer('Введите команду /start, чтобы начать общение.'))
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
