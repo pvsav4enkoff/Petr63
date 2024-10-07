74639e530caeb6af025d566dfd8beb02f372cec5
pvsav4enkoff <pvsav4enkoff@gmail.com>
2024-10-07 14:49:08 +0400

    Update module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -17,41 +17,67 @@ class State(StatesGroup):
 async def set_age(message):
     await message.answer('Введите свой возраст:')
     await State.age.set()
-
 @dp.message_handler(state = State.age)
 async def set_growth(message,state):
     await state.update_data(age=message.text)
-    await message.answer('Введите свой рост:')
-    await State.growth.set()
+    data = await state.get_data()
 
+    if data['age'].isdigit():
+        if 0 < int(data['age']) and int(data['age']) <=100:
+            await message.answer('Введите свой рост:')
+            await State.growth.set()
+        else:
+            await message.answer('Не верное значение возраст (1-100)')
+            await state.finish()
+    else:
+        await message.answer('Должна быть цифра')
+        await state.finish()
 @dp.message_handler(state = State.growth)
 async def set_weight(message, state):
     await state.update_data(growth=message.text)
-    await message.answer('Введите свой вес:')
-    await State.weight.set()
-
+    data = await state.get_data()
+    if data['growth'].isdigit():
+        if 50 < int(data['growth']) and int(data['growth']) <= 250:
+            await message.answer('Введите свой вес:')
+            await State.weight.set()
+        else:
+            await message.answer('Не верное значение рост (50 - 250)')
+            await state.finish()
+    else:
+        await message.answer('Должна быть цифра')
+        await state.finish()
 @dp.message_handler(state = State.weight)
 async def set_weight(message, state):
     await state.update_data(weigth=message.text)
-    await message.answer('Введите свой пол (1 - муж, 2 - жен):')
-    await State.sex.set()
-
+    data = await state.get_data()
+    if data['weigth'].isdigit():
+        if 30 < int(data['weigth']) and int(data['weigth']) <= 200:
+            await message.answer('Введите свой пол (1 - муж, 2 - жен):')
+            await State.sex.set()
+        else:
+            await message.answer('Не верное значение вес (30 -200)')
+            await state.finish()
+    else:
+        await message.answer('Должна быть цифра')
+        await state.finish()
 @dp.message_handler(state = State.sex)
 async def send_calories(message, state):
     await state.update_data(sex=message.text)
     data = await state.get_data()
     # для     мужчин: 10     х     вес(кг) + 6, 25    x    рост(см) – 5    х    возраст(г) + 5;
     # для     женщин: 10     х     вес(кг) + 6, 25    x    рост(см) – 5    х    возраст(г) - 161;
-    # Calories=0
-    if int(data['sex']) == 1:
-        Calories= (10*int(data['weigth']))+(6.25 * int(data['growth']))-(5*int(data['age']))+5
-        await message.answer(f"Ваша норма калорий {Calories}")
-    elif int(data['sex']) == 2:
-        Calories = (10 * int(data['weigth'])) + (6.25 * int(data['growth'])) - (5 * int(data['age']))-161
-        await message.answer(f"Ваша норма калорий {Calories}")
+    if data['sex'].isdigit():
+        if int(data['sex']) == 1:
+            Calories= (10*int(data['weigth']))+(6.25 * int(data['growth']))-(5*int(data['age']))+5
+            await message.answer(f"Ваша норма калорий {Calories}")
+        elif int(data['sex']) == 2:
+            Calories = (10 * int(data['weigth'])) + (6.25 * int(data['growth'])) - (5 * int(data['age']))-161
+            await message.answer(f"Ваша норма калорий {Calories}")
+        else:
+            await message.answer(f"Не верное значение пол (1 - муж, 2 - жен)")
     else:
-        await message.answer(f"Не верное значение пол (1 - муж, 2 - жен)")
-
+        await message.answer('Должна быть цифра')
+        await state.finish()
     await state.finish()
 
 
