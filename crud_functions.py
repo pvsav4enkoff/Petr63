2d00ab723cbe3b672ee3bba99345dcc1eeef96df
pvsav4enkoff <pvsav4enkoff@gmail.com>
2024-10-19 13:58:17 +0400

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,9 +1,4 @@
 import sqlite3
-
-# connection = sqlite3.connect("not_telegram.db")
-# cursor = connection.cursor()
-
-
 async def initiate_db():
     connection = sqlite3.connect("not_telegram.db")
     cursor = connection.cursor()
