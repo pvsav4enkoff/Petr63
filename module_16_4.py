70efd67a08f35f72fadb30ae55bec5de50422be7
pvsav4enkoff <pvsav4enkoff@gmail.com>
2024-11-07 09:21:30 +0400

    Add files via upload

---
+++ module_16_4.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, Path, HTTPException
+from typing import Annotated, List
+from pydantic import BaseModel
+
+app = FastAPI()
+
+users = []
+
+
+class User(BaseModel):
+    id: int = None
+    username: str = None
+    age: int = None
+
+
+@app.get("/users")
+async def get_all_messages() -> List[User]:
+    return users
+
+
+@app.post('/user/{username}/{age}')
+async def app_user(username: str, age: int):
+    us = User()
+    if len(users) == 0:
+        us.id = 1
+    else:
+        us.id = len(users) + 1
+    us.username = username
+    us.age = age
+    users.append(us)
+    return f"User {us.id} is registered"
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def upd_user(user_id: int, username, age: int):
+    try:
+        if user_id not in users:
+            users[user_id - 1] = {}
+        users[user_id - 1]["id"] = user_id
+        users[user_id - 1]["username"] = username
+        users[user_id - 1]["age"] = age
+        return f"User {user_id}  has been updated"
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.delete('/user/{user_id}')
+async def del_user(user_id: int):
+    try:
+        del users[user_id - 1]
+        return f"User {user_id} has been deleted"
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
