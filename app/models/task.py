a7e18aedcd7e93c2f05100425f299867888e9d52
pvsav4enkoff <pvsav4enkoff@gmail.com>
2024-11-22 12:39:40 +0400

    Add files via upload

--- app/models/task.py
+++ app/models/task.py
@@ -1,19 +1,19 @@
 from app.backend.db import Base
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
 from app.models.user import User
-from user import User
 class Task(Base):
     __tablename__ = 'tasks'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True, index=True)
     title = Column(String)
     content = Column(String)
     priority = Column(Integer, default=0)
     completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey("users.id"), index=True, nullable=False)
+    user_id = Column(Integer, ForeignKey(User.__tablename__ + '.id'), index=True, nullable=False)
     slug = Column(String, index=True, unique=True)
 
-
-    user = relationship("users", back_populates='tasks')
+    users = relationship('User', backref='tasks')
+    # users = relationship('User', back_populates='Task')
 
 from sqlalchemy.schema import CreateTable
 print(CreateTable(Task.__table__))
--- app/models/user.py
+++ app/models/user.py
@@ -1,18 +1,18 @@
 from app.backend.db import Base
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship
-# from tasks import Task
+# from app.models.task import Task
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True, index=True)
     username = Column(String)
     firstname = Column(String)
     lastname = Column(String)
     age = Column(Integer)
     slug = Column(String, index=True, unique=True)
 
-    tasks = relationship("tasks", back_populates='users')
+    task = relationship('Task', back_populates='users')
 
 from sqlalchemy.schema import CreateTable
 print(CreateTable(User.__table__))
